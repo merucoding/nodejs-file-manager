d42fae95409ed592f8feb107434a575a6740fb6b
Meruert <mika.amantaikuzu@gmail.com>
2024-10-12 20:03:07 +0500

    feat: implement cd

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { argv, stdin, stdout } from "node:process";
 import path from "path";
 import os from "os";
+import fs from "fs/promises";
 
 const homeDir = os.homedir();
 const rootDir = path.parse(process.cwd()).root;
@@ -16,13 +17,17 @@ console.log(`Welcome to the File Manager, ${userName[0]}!`);
 console.log(`\nYou are currently in ${process.cwd()}`);
 
 stdin.on("data", (data) => {
-  const input = data.toString().trim();
+  const input = data.toString();
 
-  if (input === 'up') {
+  if (input.trim() === "up") {
     upFromCurrDir();
   }
 
-  if (input === ".exit") {
+  if (input.startsWith("cd ")) {
+    cdToDir(input);
+  }
+
+  if (input.trim() === ".exit") {
     console.log(`Thank you for using File Manager, ${userName[0]}, goodbye!`);
     process.exit();
   }
@@ -36,11 +41,36 @@ process.on("SIGINT", () => {
 function upFromCurrDir() {
   const currPath = process.cwd();
   const newPath = path.dirname(currPath);
+  console.log(newPath);
 
   if (currPath === rootDir) {
     console.log(`\nYou are currently in ${process.cwd()}`);
   } else {
     process.chdir(newPath);
     console.log(`\nYou are currently in ${process.cwd()}`);
+  }
+}
+
+async function cdToDir(input) {
+  const inputPath = input.slice(3).trim();
+
+  if (path.isAbsolute(inputPath)) {
+    try {
+      await fs.access(inputPath);
+    } catch {
+      console.log("Operation failed");
+    }
+    process.chdir(inputPath);
+    console.log(process.cwd());
+  } else {
+    const relativePath = path.resolve(process.cwd(), inputPath);
+
+    try {
+      await fs.access(relativePath);
+    } catch {
+      console.log("Operation failed");
+    }
+    process.chdir(relativePath);
+    console.log(process.cwd());
   }
 }
