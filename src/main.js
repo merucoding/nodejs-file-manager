53e3d9e09b3f6f6fbbf6196167665b98f25903c2
Meruert <mika.amantaikuzu@gmail.com>
2024-10-12 16:11:20 +0500

    feat: implement up

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { argv, stdin, stdout } from "node:process";
+import path from "path";
+import os from "os";
+
+const homeDir = os.homedir();
+const rootDir = path.parse(process.cwd()).root;
+process.chdir(homeDir);
 
 const arr = argv.slice(2);
 
@@ -7,9 +13,16 @@ const userName = arr
   .map((el) => el.slice(11));
 
 console.log(`Welcome to the File Manager, ${userName[0]}!`);
+console.log(`\nYou are currently in ${process.cwd()}`);
 
 stdin.on("data", (data) => {
-  if (data.includes(".exit")) {
+  const input = data.toString().trim();
+
+  if (input === 'up') {
+    upFromCurrDir();
+  }
+
+  if (input === ".exit") {
     console.log(`Thank you for using File Manager, ${userName[0]}, goodbye!`);
     process.exit();
   }
@@ -19,3 +32,15 @@ process.on("SIGINT", () => {
   console.log(`\nThank you for using File Manager, ${userName[0]}, goodbye!`);
   process.exit();
 });
+
+function upFromCurrDir() {
+  const currPath = process.cwd();
+  const newPath = path.dirname(currPath);
+
+  if (currPath === rootDir) {
+    console.log(`\nYou are currently in ${process.cwd()}`);
+  } else {
+    process.chdir(newPath);
+    console.log(`\nYou are currently in ${process.cwd()}`);
+  }
+}
