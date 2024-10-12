3c3b11927ec73f3ec904af4a04be7aa54728540d
Meruert <mika.amantaikuzu@gmail.com>
2024-10-12 14:20:22 +0500

    feat: implement General subtask

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { argv, stdin, stdout } from "node:process";
+
+const arr = argv.slice(2);
+
+const userName = arr
+  .filter((el) => el.startsWith("--username="))
+  .map((el) => el.slice(11));
+
+console.log(`Welcome to the File Manager, ${userName[0]}!`);
+
+stdin.on("data", (data) => {
+  if (data.includes(".exit")) {
+    console.log(`Thank you for using File Manager, ${userName[0]}, goodbye!`);
+    process.exit();
+  }
+});
+
+process.on("SIGINT", () => {
+  console.log(`\nThank you for using File Manager, ${userName[0]}, goodbye!`);
+  process.exit();
+});
