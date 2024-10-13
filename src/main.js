0d7169ade4839a4c3b32e259c7a81d41a07b77ca
Meruert <mika.amantaikuzu@gmail.com>
2024-10-13 13:38:40 +0500

    feat: finalize cd to dir

--- src/main.js
+++ src/main.js
@@ -52,25 +52,36 @@ function upFromCurrDir() {
 }
 
 async function cdToDir(input) {
-  const inputPath = input.slice(3).trim();
+  const inputPath = input.slice(3).replace(/[\n\r]/g, ''); // убираем переносы строк
+  let finalPath = ''; 
 
-  if (path.isAbsolute(inputPath)) {
-    try {
-      await fs.access(inputPath);
-    } catch {
-      console.log("Operation failed");
-    }
-    process.chdir(inputPath);
-    console.log(process.cwd());
+  if (path.isAbsolute(inputPath) && inputPath.startsWith(rootDir)) {
+    finalPath = inputPath;
   } else {
-    const relativePath = path.resolve(process.cwd(), inputPath);
+    finalPath = path.resolve(process.cwd(), inputPath);
+  }
 
-    try {
-      await fs.access(relativePath);
-    } catch {
-      console.log("Operation failed");
+  try {
+    await fs.access(inputPath);
+
+    const stats = await fs.stat(inputPath);
+    if (stats.isDirectory()) {
+      // console.log('ok', finalPath);
+      process.chdir(inputPath);
+      console.log(`\nYou are currently in ${process.cwd()}`);
+    } else {
+      console.log('Invalid input: not a folder', finalPath);
     }
-    process.chdir(relativePath);
-    console.log(process.cwd());
+  } catch {
+    console.log('Invalid input: no access', finalPath);
   }
 }
+
+// /Users/meruert.amantay/Desktop/node/nodejs-file-manager/src/etc.js no
+// /Users/meruert.amantay/Desktop/node/nodejs-file-manager/src ok
+// meruert.amantay/Desktop/node/nodejs-file-manager/src/etc.js no
+// nodejs-file-manager/src/etc.js no
+// Desktop/node/nodejs-file-manager/src/ ok
+// Desktop/node/nodejs-file-manager/src/etc.js no
+// /Users/meruert.amantay/Desktop/  v   / ok
+// v no
